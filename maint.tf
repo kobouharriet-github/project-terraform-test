06ada5c0f824dab27033c57b6f9f67e28c9785ba
Kevin <kobouharriet@gmail.com>
2021-04-12 21:15:27 -0300

    Zone Habilitada en subnet

--- maint.tf
+++ maint.tf
@@ -1,5 +1,6 @@
 provider "aws" {
   profile = "MyAWS"
+  region = "us-east-1"
 }
 
 
@@ -7,4 +8,18 @@ resource "aws_vpc" "my-vpc" {
 
 cidr_block = "10.0.0.0/16"
 
+    tags = {
+    Name = "my-vpc"
+    }
+}
+
+resource "aws_subnet" "my-subnet-public" {
+  vpc_id     = aws_vpc.my-vpc.id
+  cidr_block = "10.0.0.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = "us-east-1"
+
+  tags = {
+    Name = "my-subnet-public"
+  }
 }
